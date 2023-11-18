79d570fd87f05532068503e96cc36672e97adc50
marin-mi <marin0109yuki@gmail.com>
2023-11-18 19:49:27 +0900

    main

---
+++ recognize.py
@@ -0,0 +1,20 @@
+import whisper
+
+
+def transcribe_audio(file_path):
+    # Whisperモデルをロード
+    model = whisper.load_model("tiny")
+
+    # 指定された音声ファイルを文字起こし
+    result = model.transcribe(file_path, verbose=True, language='ja')
+
+    # 結果からテキストを取得
+    text = result['text']
+
+    return text
+
+
+# 使用例
+file_name = "test1.m4a"
+transcribed_text = transcribe_audio(file_name)
+print(transcribed_text)

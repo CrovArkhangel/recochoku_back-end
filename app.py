c5c5f126cd7365e99307772991a69ef470453eca
marin-mi <marin0109yuki@gmail.com>
2023-11-19 15:30:00 +0900

    [add] something

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 from flask import Flask
 from flask import request
-from flask import make_response
+from flask import jsonify
 from flask_cors import CORS
 
 import recognize
-import translate
 
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/recognize/voice", methods=["POST"])
 def transcription():
-    data_voice = request.files['files']
-    return recognize.recognize(data_voice), 200
+    print(request.get_data())
+    print("request test")
+
+    data_voice = request.get_data()
+    path = 'input.wav'
+    f = open(path, 'wb')
+    f.write(data_voice)
+    f.close()
+    
+    result = recognize.transcribe_audio(path)
+    print(result)
+    return jsonify({"text": result}), 200
 
 # @app.route("/translate/voice", method=["POST"])
 # def translate():
-    
-
-
-
-    
--- recognize.py
+++ recognize.py
@@ -2,15 +2,16 @@ import whisper
 
 
 def transcribe_audio(file_path):
+    print("check out")
     # Whisperモデルをロード
-    model = whisper.load_model("large")
+    model = whisper.load_model("tiny")
 
     # 指定された音声ファイルを文字起こし
     result = model.transcribe(file_path, verbose=True, language='ja')
-
+    print(result)
     # 結果からテキストを取得
     text = result['text']
-
+    print(text)
     return text
 
 

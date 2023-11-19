33146f337f2dafbe5625fd40cb8038def2bf7da9
takumi-333 <takumi_soccer0514@icloud.com>
2023-11-19 10:21:55 +0900

    CORS対処

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask_cors import CORS
 
 import recognize
 import translate
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
